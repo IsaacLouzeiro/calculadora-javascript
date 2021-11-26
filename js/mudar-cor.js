963b42ef3ab2b72c6eed85bb24a28d202a7b63c4
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-11-26 17:23:43 -0300

    deixando menu de modos, funcional

---
+++ js/mudar-cor.js
@@ -0,0 +1,28 @@
+function mudarCor(cor) {
+    var geral = document.getElementById("geral")
+
+    var itemClaro = document.getElementById('item-claro')
+
+    var itemEscuro = document.getElementById('item-escuro')
+
+    switch (cor) {
+        case "claro":
+            geral.classList.add('claro')
+            geral.classList.remove('escuro')
+
+            itemClaro.classList.add("active")
+            itemEscuro.classList.remove("active")
+            break;
+
+        case "escuro":
+            geral.classList.add('escuro')
+            geral.classList.remove('claro')
+
+            itemClaro.classList.remove("active")
+            itemEscuro.classList.add("active")
+            break;
+    
+        default:
+            break;
+    }
+}
