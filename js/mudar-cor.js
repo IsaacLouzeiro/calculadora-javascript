370a6a0b97bfe069c32f1baf7bc8a9384b16cd6b
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-11-30 17:06:04 -0300

    deixando salvo a opção de tema

--- js/mudar-cor.js
+++ js/mudar-cor.js
@@ -1,26 +1,69 @@
-function mudarCor(cor) {
-    var geral = document.getElementById("geral")
+// checar se o modo está ativo
+const memColorLight = localStorage.getItem('claro');
+const memColorDark = localStorage.getItem('escuro');
 
-    var itemClaro = document.getElementById('item-claro')
+// deixar o item escuro por padrão
+document.getElementById("item-escuro").classList.add("active");
+document.getElementById("geral").classList.add("escuro");
 
-    var itemEscuro = document.getElementById('item-escuro')
+// se o modo claro estiver no local storage
+if (memColorLight) {
+    document.getElementById("item-claro").classList.add("active");
+    document.getElementById("item-escuro").classList.remove("active");
+    document.getElementById("geral").classList.add("claro");
+    document.getElementById("geral").classList.remove("escuro");
+}
 
+// se o modo escuro estiver no local storage
+if (memColorDark) {
+    document.getElementById("item-escuro").classList.add("active");
+    document.getElementById("item-claro").classList.remove("active");
+    document.getElementById("geral").classList.add("escuro");
+    document.getElementById("geral").classList.remove("claro");
+}
+
+function mudarCor(btn, cor) {
+    location.reload();
+    var geral = document.getElementById("geral");
     switch (cor) {
-        // inserir classe "x" se o botao "x" for pressionado
         case "claro":
-            geral.classList.add('claro')
-            geral.classList.remove('escuro')
+            // remover o modo do localStorage
+            localStorage.removeItem('escuro');
+            // inserir a classe active
+            btn.classList.add("active");
+            
+            geral.classList.add("claro");
 
-            itemClaro.classList.add("active")
-            itemEscuro.classList.remove("active")
+            // se tiver a classe active
+            if (btn.classList.contains('active')) {
+                // adicionar o tema no localStorage
+                localStorage.setItem('claro', true)
+                geral.classList.add("claro");
+                return
+            }
+            // se não, remove
+            localStorage.removeItem('claro');
+            
             break;
 
         case "escuro":
-            geral.classList.add('escuro')
-            geral.classList.remove('claro')
+            // remover o modo do localStorage
+            localStorage.removeItem('claro');
+            // inserir a classe active
+            btn.classList.add("active");
 
-            itemClaro.classList.remove("active")
-            itemEscuro.classList.add("active")
+            geral.classList.add("escuro");
+
+            // se tiver a classe active
+            if (btn.classList.contains('active')) {
+                // adicionar o tema no localStorage
+                localStorage.setItem('escuro', true)
+                geral.classList.add("escuro");
+                return
+            }
+            // se não, remove
+            localStorage.removeItem('escuro');
+
             break;
     
         default:
